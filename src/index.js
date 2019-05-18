9423ba4f0ac0caa018fa17f24eb6d1a06263c309
yjdjiayou <674032206@qq.com>
2019-05-19 01:18:29 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Home from './components/Home';
+import Counter from './components/Counter';
+import {Route,Link} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store,persistor} from './store';
+import {ConnectedRouter} from './connected-react-router';
+import history from './store/history';
+import { PersistGate } from 'redux-persist/integration/react'
+ReactDOM.render(<Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
+    <ConnectedRouter history={history}>
+    <>
+       <Link to="/">Home</Link>
+       <Link to="/counter">Counter</Link>
+       <hr/>
+       <Route path="/" exact={true} component={Home}/>
+       <Route path="/counter" component={Counter}/>
+    </>
+   </ConnectedRouter>
+   </PersistGate>
+</Provider>, document.getElementById('root'));
