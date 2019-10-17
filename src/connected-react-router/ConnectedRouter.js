4edf3f80dcae8b311ded991385246c1c759de631
yaojiadong <674032206@qq.com>
2019-10-17 16:41:44 +0800

    新增注释

--- src/connected-react-router/ConnectedRouter.js
+++ src/connected-react-router/ConnectedRouter.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import {LOCATION_CHANGE} from './constants';
 import {ReactReduxContext} from 'react-redux';
-import {Router} from 'react-router';//react-router-dom是浏览器端的Router
-//hashRouter 
+// 注意：这里引入的是 react-router
+// react-router 实现了路由的核心功能，可以看做是一个路由的基础库，其他路由库都是基于它实现的
+// 而 react-router-dom 是浏览器端的 Router
+// react-router-dom 是基于 react-router，加入了浏览器操作的一些功能。
+// react-router-dom 是浏览器端的 Router
+import {Router} from 'react-router';
+
+// ConnectedRouter 内部其实也是用到了 react-router 中的 Router，自身就只是多了一个监听的功能
 export default class ConnectedRouter extends Component {
   static contextType = ReactReduxContext
   componentDidMount(){
-      //每当路径发生变化之后，都会执行此监听函数，并传入二个参数新的路径的新的动作
+      // 每当路径发生变化之后，都会执行此监听函数，并传入2个参数：新的路径、新的动作
+      // 这里的 action 和 redux 中的 action 没有任何关系，就是一个字符串，常见的有（"PUSH","POP"）
      this.unlistener = this.props.history.listen((location,action)=>{
-        this.context.store.dispatch({
+         // 这里的 dispatch 方法是仓库的方法，而仓库是通过上下文获取的
+         this.context.store.dispatch({
             type:LOCATION_CHANGE,
             payload:{location,action}
         });
@@ -19,7 +27,7 @@ export default class ConnectedRouter extends Component {
   }
   render() {
     let {history,children} = this.props;
-    // HashRouter 向下层传递history location match
+    // HashRouter 向下层传递 history location match
     return (
       <Router history={history}>
           {children}
@@ -27,7 +35,8 @@ export default class ConnectedRouter extends Component {
     )
   }
 }
+
 /**
- * HashRouter就是一个拥有了hashhistory的ReactRouter
+ * HashRouter就是一个拥有了 hashhistory 的 ReactRouter
  * <Router history={createHashHistory()}><Router>
  */
